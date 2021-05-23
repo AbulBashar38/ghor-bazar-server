464be61b899410637d2365f69a85ad8e3e7d2966
unknown <you@example.com>
2021-05-23 12:25:32 +0600

    add one product from clint success

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rdsgc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
@@ -15,8 +15,13 @@ app.use(bodyParser.json())
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION}`);
-  console.log('database connect');
+  const productCollections = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION}`);
+  app.post('/addProductInDatabase',(req,res)=>{
+    productCollections.insertOne(req.body)
+    .then(result=>{
+      res.send(result.insertedCount>0)
+    })
+  })
 //   client.close();
 });
 
